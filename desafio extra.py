8d504c666413c848c1f91830a3b8b8c3a06e3eaf
Gabriel dos Santos <Gsilva.job@gmail.com>
2022-08-03 09:35:25 -0300

    teste branch

--- desafio extra.py
+++ desafio extra.py
@@ -2,4 +2,5 @@ tabuada=int(input("Digite um número para exibir a tabuada: "))
 print("Tabuada do número: ", tabuada)
 for valor in range(1,11,1):
     print("{0} x {1} = {2}".format(str(tabuada), str(valor), str((tabuada * valor))))
+    #add modificação para teste de branch
     #print(str(tabuada) + " x " + str(valor) + " = " + str((tabuada*valor)))
